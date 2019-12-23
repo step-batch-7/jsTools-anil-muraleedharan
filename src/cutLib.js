2f9e8e48a27ccae906325d02ecc6dde8b8baa20f
Anil E M <anilmuraleedharan@In-Anile.local>
2019-12-23 10:01:46 +0530

    added parseUserOptions function with test

--- src/cutLib.js
+++ src/cutLib.js
@@ -1,1 +1,16 @@
+const parseUserInputs = function(cmdLineArgs) {
+  const requiredArgs = cmdLineArgs.slice(2);
+  const argsLength = requiredArgs.length;
+  const userOptions = {};
+  const indexOfFields = requiredArgs.indexOf('-f') + 1;
+  const indexOfDelimiter = requiredArgs.indexOf('-d') + 1;
 
+  userOptions.fields = requiredArgs[indexOfFields].split(',');
+  userOptions.delimiter = requiredArgs[indexOfDelimiter];
+  if (argsLength % 2 != 0) {
+    userOptions.path = requiredArgs[argsLength - 1];
+  }
+  return userOptions;
+};
+
+module.exports = { parseUserInputs };
--- test/cutLibTest.js
+++ test/cutLibTest.js
@@ -1,2 +1,31 @@
 const { deepStrictEqual } = require('chai').assert;
-const {} = require('../src/cutLib');
+const { parseUserInputs } = require('../src/cutLib');
+
+describe('parseUserInputs', function() {
+  it('should give an object of required user inputs', function() {
+    const userInput = [
+      'node',
+      'cut.js',
+      '-f',
+      '1-2,4,5',
+      '-d',
+      ',',
+      './tmp1.txt'
+    ];
+    const expected = {
+      path: './tmp1.txt',
+      fields: ['1-2', '4', '5'],
+      delimiter: ','
+    };
+    deepStrictEqual(parseUserInputs(userInput), expected);
+  });
+
+  it('should give an object of required user inputs when the path is not specified', function() {
+    const userInput = ['node', 'cut.js', '-f', '1-2,4,5', '-d', ','];
+    const expected = {
+      fields: ['1-2', '4', '5'],
+      delimiter: ','
+    };
+    deepStrictEqual(parseUserInputs(userInput), expected);
+  });
+});
