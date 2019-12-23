6732c8bd72d8b341a7ac222bb46c0dcdb8b1bb14
Anil E M <anilmuraleedharan@In-Anile.local>
2019-12-23 10:09:03 +0530

    added readFileContents Function

--- src/cutLib.js
+++ src/cutLib.js
@@ -1,16 +1,23 @@
 const parseUserOptions = function(cmdLineArgs) {
   const requiredArgs = cmdLineArgs.slice(2);
   const argsLength = requiredArgs.length;
-  const userOptions = {};
+  const parsedOptions = {};
   const indexOfFields = requiredArgs.indexOf('-f') + 1;
   const indexOfDelimiter = requiredArgs.indexOf('-d') + 1;
 
-  userOptions.fields = requiredArgs[indexOfFields].split(',');
-  userOptions.delimiter = requiredArgs[indexOfDelimiter];
+  parsedOptions.fields = requiredArgs[indexOfFields].split(',');
+  parsedOptions.delimiter = requiredArgs[indexOfDelimiter];
   if (argsLength % 2 != 0) {
-    userOptions.path = requiredArgs[argsLength - 1];
+    parsedOptions.path = requiredArgs[argsLength - 1];
   }
-  return userOptions;
+  return parsedOptions;
 };
 
-module.exports = { parseUserOptions };
+const readFileContents = function(fileReader, existenceChecker, path) {
+  if (existenceChecker(path)) {
+    return { fileExistence: true, contents: fileReader(path).split('\n') };
+  }
+  return { fileExistence: false, contents: [] };
+};
+
+module.exports = { parseUserOptions, readFileContents };
--- test/cutLibTest.js
+++ test/cutLibTest.js
@@ -1,9 +1,9 @@
 const { deepStrictEqual } = require('chai').assert;
-const { parseUserOptions } = require('../src/cutLib');
+const { parseUserOptions, readFileContents } = require('../src/cutLib');
 
 describe('parseUserOptions', function() {
   it('should give an object of required user Options', function() {
-    const userInput = [
+    const userOption = [
       'node',
       'cut.js',
       '-f',
@@ -17,15 +17,38 @@ describe('parseUserOptions', function() {
       fields: ['1-2', '4', '5'],
       delimiter: ','
     };
-    deepStrictEqual(parseUserOptions(userInput), expected);
+    deepStrictEqual(parseUserOptions(userOption), expected);
   });
 
   it('should give an object of required user Options when the path is not specified', function() {
-    const userOptions = ['node', 'cut.js', '-f', '1-2,4,5', '-d', ','];
+    const userOption = ['node', 'cut.js', '-f', '1-2,4,5', '-d', ','];
     const expected = {
       fields: ['1-2', '4', '5'],
       delimiter: ','
     };
-    deepStrictEqual(parseUserOptions(userOptions), expected);
+    deepStrictEqual(parseUserOptions(userOption), expected);
+  });
+});
+
+describe('readFileContents', function() {
+  const existenceChecker = path => path === 'path';
+  const fileReader = path => {
+    return 'line1\nline2\nline3';
+  };
+  it('should read the contents of the file if it is exist and the file existence should be truthy', function() {
+    deepStrictEqual(readFileContents(fileReader, existenceChecker, 'path'), {
+      fileExistence: true,
+      contents: ['line1', 'line2', 'line3']
+    });
+  });
+
+  it('should read ', function() {
+    deepStrictEqual(
+      readFileContents(fileReader, existenceChecker, 'wrong path'),
+      {
+        fileExistence: false,
+        contents: []
+      }
+    );
   });
 });
