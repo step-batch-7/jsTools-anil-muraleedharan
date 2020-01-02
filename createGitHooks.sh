ca2944159de8b96234c812d71edc57007da1c0df
Anil E M <56071561+anil-muraleedharan@users.noreply.github.com>
2020-01-02 23:39:49 +0530

    Added a shell script for creting pre commit hook

--- createGitHooks.sh
+++ createGitHooks.sh
@@ -1,25 +1,50 @@
 #!/bin/bash
+  
+echo '#!/bin/bash
 
-cat <<EOF > .git/hooks/pre-commit  
-npm run allTest 
-if [ \$? != 0 ]; then 
+red=`tput setaf 1`
+reset=`tput sgr0`
+./runAppTests.sh appTests/*.test > appTestOut.txt
+cat appTestOut.txt | grep '❌' > appTestError.txt
+if [ $? == 0 ]; then 
+    tput bold
+    echo "${red}❌ Fix the AppTests${reset} ❌"
+    cat appTestError.txt
+    rm appTestOut.txt appTestError.txt
     exit 1
 fi
-grep -rnw '//' ./src/*.js ./test/*.js ./*.js 
-if [ \$? == 0 ]; then
+rm appTestOut.txt appTestError.txt
+
+nyc mocha > mochaOutput.txt
+if [ $? != 0 ]; then
+    tput bold
+    echo "${red}❌ Fix the unit test${reset} ❌"
+    cat mochaOutput.txt
+    rm mochaOutput.txt
     exit 1
 fi
-EOF
+rm mochaOutput.txt
 
-
-chmod +x .git/hooks/pre-commit  
-
-cat <<EOF > .git/hooks/pre-push  
-npm run lint 
-if [ \$? != 0 ]; then 
+eslint src test > eslintError.txt
+if [ $? != 0 ]; then 
+    tput bold
+    echo "${red}❌ Follow ESLint standards${reset} ❌"
+    cat eslintError.txt
+    rm eslintError.txt
     exit 1
 fi
-EOF
+rm eslintError.txt
 
+grep -rn "//" ./src/*.js ./test/*.js ./*.js > CommendedLines.txt
+if [ $? == 0 ]; then
+    tput bold   
+    echo "${red} ❌ Remove the unnecessary lines${reset} ❌"
+    count=$(cat CommendedLines.txt | wc -l)
+    echo "Total Lines Commended : " $count  
+    cat -n CommendedLines.txt
+    rm CommendedLines.txt
+    exit 1
+fi
+rm CommendedLines.txt'> .git/hooks/pre-commit
 
-chmod +x .git/hooks/pre-push  
+chmod +x .git/hooks/pre-commit
